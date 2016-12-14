9ff0a5bc77a1011b9557b643918c9347fc3501b2
Pasalapudi <paditya@amazon.com>
2016-12-14 15:27:00 +0530

    Initial Commit

---
+++ PitchPerfectSubmission/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PitchPerfectSubmission
+//
+//  Created by Pasalapudi, Manikanta Aditya on 12/14/16.
+//  Copyright © 2016 Pasalapudi, Manikanta Aditya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
